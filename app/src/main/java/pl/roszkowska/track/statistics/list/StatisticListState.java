c53042a1b531d195abe9152db274b3ee5f6366fa
mike.kulasinski <mike.kulasinski@gmail.com>
2018-12-17 17:11:27 +0100

    dodalam loading screen :)

--- app/src/main/java/pl/roszkowska/track/statistics/list/StatisticListState.java
+++ app/src/main/java/pl/roszkowska/track/statistics/list/StatisticListState.java
@@ -2,10 +2,11 @@ package pl.roszkowska.track.statistics.list;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class StatisticListState {
 
-    public boolean isLoading;
+    public boolean isLoading = true;
     public List<Item> itemList = new ArrayList<>();
 
     public static class Item {
@@ -18,5 +19,19 @@ public class StatisticListState {
             this.routeDuration = routeDuration;
             this.timestamp = timestamp;
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StatisticListState that = (StatisticListState) o;
+        return isLoading == that.isLoading &&
+                Objects.equals(itemList, that.itemList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isLoading, itemList);
     }
 }
--- app/src/main/java/pl/roszkowska/track/ui/statistics/list/StatisticsActivity.java
+++ app/src/main/java/pl/roszkowska/track/ui/statistics/list/StatisticsActivity.java
@@ -4,31 +4,51 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.ViewSwitcher;
 
 import io.reactivex.disposables.CompositeDisposable;
 import pl.roszkowska.track.R;
 import pl.roszkowska.track.module.TrackModule;
 import pl.roszkowska.track.statistics.list.StatisticListEvent;
+import pl.roszkowska.track.statistics.list.StatisticListState;
 
 public class StatisticsActivity extends AppCompatActivity {
 
+    private static final int VIEW_LOADING = 0;
+    private static final int VIEW_LIST = 1;
+
     private CompositeDisposable mDisposable = new CompositeDisposable();
+    private StatisticsAdapter mAdapter;
+    private ViewSwitcher mViewSwitcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistics_recycler_view);
 
+        mAdapter = new StatisticsAdapter();
+
+        mViewSwitcher = findViewById(R.id.viewSwitcherListOfRoutes);
+
         RecyclerView recyclerView = findViewById(R.id.recyclerViewListOfRoutes);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        StatisticsAdapter adapter = new StatisticsAdapter();
-        recyclerView.setAdapter(adapter);
+        recyclerView.setAdapter(mAdapter);
 
         mDisposable.add(TrackModule
                 .statisticListStateStream()
-                .subscribe(state -> adapter.setModels(state.itemList))
+                .distinctUntilChanged()
+                .subscribe(this::bind)
         );
+    }
+
+    private void bind(StatisticListState state) {
+        if (state.isLoading) {
+            mViewSwitcher.setDisplayedChild(VIEW_LOADING);
+        } else {
+            mViewSwitcher.setDisplayedChild(VIEW_LIST);
+            mAdapter.setModels(state.itemList);
+        }
     }
 
     @Override
